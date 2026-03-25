0f9cd7d1ad085fd535160ced7a37e87b57601dd4
Asmah Fatimah <asmahfatima7@gmail.com>
2026-03-25 23:41:52 +0500

    Update SiteContext.tsx

--- src/context/SiteContext.tsx
+++ src/context/SiteContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react'
+import { createContext, useContext } from 'react'
 import type { Site } from '../types/Site';
 
 export const SiteContext = createContext<Site | null>(null);
