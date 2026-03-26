de4690f71d34bb7721bf71d452c6bed3a337656c
Asmah Fatimah <asmahfatima7@gmail.com>
2026-03-26 16:43:33 +0500

    Update Section.tsx

--- src/section/Section.tsx
+++ src/section/Section.tsx
@@ -1,24 +1,28 @@
-import type { ReactNode, ForwardedRef  } from 'react'
+import type { ReactNode, ForwardedRef } from "react";
 import { forwardRef } from "react";
-interface Props{
-    id:string,
-    title: string,
-    subTitle: string,
-    children?: ReactNode,
-    className?: string
 
+interface Props {
+  id: string;
+  title?: string;
+  subTitle?: string;
+  children?: ReactNode;
+  className?: string;
 }
-const Section = forwardRef<HTMLDivElement, Props>( ({ id, title, subTitle, children, className }, ref: ForwardedRef<HTMLDivElement>)) => {
-  return (
-    <section className={`flex flex-col  justify-center items-center p-4 lg:px-36 py-12 min-h-[200px] sm:min-h-[280px] ${className}`} ref={ref} id={id}>
-        {title &&  <h3 className="font-bold text-2xl mb-2">{title}</h3>}
-   
-        {subTitle && <h4 className='text-center max-w-lg'>{subTitle}</h4>
-        
-        }
+
+const Section = forwardRef<HTMLDivElement, Props>(
+  ({ id, title, subTitle, children, className }, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+      <section
+        id={id}
+        ref={ref} // attach forwarded ref for scrolling
+        className={`flex flex-col justify-center items-center p-4 lg:px-36 py-12 min-h-[200px] sm:min-h-[280px] ${className || ""}`}
+      >
+        {title && <h3 className="font-bold text-2xl mb-2">{title}</h3>}
+        {subTitle && <h4 className="text-center max-w-lg">{subTitle}</h4>}
         {children}
-    </section>
-  )
-}
+      </section>
+    );
+  }
+);
 
-export default Section
+export default Section;
