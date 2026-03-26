50b803c5b2cf4f3adce321cbea19b908e3ae6c5d
Asmah Fatimah <asmahfatima7@gmail.com>
2026-03-26 16:40:52 +0500

    Update Section.tsx

--- src/section/Section.tsx
+++ src/section/Section.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from 'react'
+import type { ReactNode, ForwardedRef  } from 'react'
 import { forwardRef } from "react";
 interface Props{
     id:string,
@@ -8,7 +8,7 @@ interface Props{
     className?: string
 
 }
-const Section = forwardRef<HTMLDivElement, SectionProps>)( ({ id, title, subTitle, children, className } : Props, ref) => {
+const Section = forwardRef<HTMLDivElement, Props>( ({ id, title, subTitle, children, className }, ref: ForwardedRef<HTMLDivElement>)) => {
   return (
     <section className={`flex flex-col  justify-center items-center p-4 lg:px-36 py-12 min-h-[200px] sm:min-h-[280px] ${className}`} ref={ref} id={id}>
         {title &&  <h3 className="font-bold text-2xl mb-2">{title}</h3>}
@@ -19,6 +19,6 @@ const Section = forwardRef<HTMLDivElement, SectionProps>)( ({ id, title, subTitl
         {children}
     </section>
   )
-})
+}
 
 export default Section
